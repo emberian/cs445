7f1e4102d820ac2257362b9cafdc13673ad4074a
Corey Richardson <corey@octayn.net>
2015-02-22 23:05:17 -0500

    translate wip

--- dragon/translate.c
+++ dragon/translate.c
@@ -7,7 +7,23 @@ static struct rec_layout only_rec = { 8, 8 };
 
 void free_ir(struct ir *i) { }
 
-struct ir *translate(struct ast_program *prog, struct stab *st) {
+struct cir_prog *translate(struct ast_program *prog, struct stab *st) {
+    struct cir_prog *p = cir_prog();
+    struct cir_func *m = cir_func();
+
+    struct cir_bb *b = cir_bb();
+
+    // bb for the global vars.
+    LFOREACH(struct ast_decls *d, prog->decls)
+        LFOREACH(char *var, d->names)
+            struct stab_var *sv = STAB_VAR(st, stab_resolve_var(st, var));
+            struct insn *slot = insn(IALLOC, size_of_type(st, sv));
+            sv->where = slot;
+            ptrvec_push(b->insns, slot);
+        ENDLFOREACH;
+    ENDLFOREACH;
+
+
     // we walk the AST. as we do so, we do name resolution and type checking.
     // menwhile, we are emiting an IR
     return NULL;
--- dragon/translate.h
+++ dragon/translate.h
@@ -13,9 +13,13 @@
 
 struct stab;
 
-struct ir { };
+struct sizing {
+    int32_t size, align;
+};
+
 struct rec_layout {
-    int64_t size, align;
+    struct sizing overall;
+    ptrvec *fields;
 };
 
 struct ir *translate(struct ast_program *, struct stab *);
@@ -23,5 +27,137 @@ void free_ir(struct ir *);
 
 void free_rec_layout(struct rec_layout *);
 struct rec_layout *compute_rec_layout(struct stab *, struct list *);
+
+/*
+ * Overall Design
+ * ==============
+ *
+ * The program is represented as a digraph of functions, which is the
+ * callgraph of the program. Each function is a control flow graph (CFG) of
+ * basic blocks (BBS). The program after translation is in SSA form, and each
+ * pass until codegen must preserve the SSA invariants. To get there, the
+ * program is translated into a straightforward CFG not in SSA form. Then, the
+ * algorithm of Cytron et. al is used to compute a minimal SSA and dependence
+ * graph.
+ *
+ * Function Environments and Capture Groups
+ * ========================================
+ *
+ * When considering the (static) call graph of the program, all functions may
+ * access the local variables of its predecessors. This is somewhat
+ * challenging to represent in a reusable IR such as this. The approach taken
+ * is to add an argument to every function which either is or dominates a
+ * function which captures nonlocals. The translator creates an aggregate type
+ * for all captured locals of a function, and stores these in memory. These
+ * aggregates, known as capture groups (CGs), also have a field for a pointer
+ * to the parent group, as well as a unique ID identifying the function the CG
+ * belongs to. Then, when a subfunction needs to access a nonlocal, it can
+ * walk up the linked list of CGs until it finds the ID corresponding to the
+ * function it needs, and read/write the proper field.
+ *
+ * CIR - Common IR
+ * ===============
+ *
+ * CIR is the linearization of the program's CFG. Used for transformation
+ * passes and codegen.
+ */
+
+enum cir_op {
+    // return from the function.
+    IRET,
+    // a conditional branch, "br A B C" reads "if C jump to BB B, else jump to
+    // BB C"
+    IBR,
+
+    // C = A + B
+    IADD,
+    // C = A - B
+    ISUB,
+    // C = A * B
+    IMUL,
+    // C = A / B
+    IDIV,
+    // C = A % B
+    IMOD,
+
+    // B = ~A
+    INOT,
+    // C = A & B
+    IAND,
+    // C = A | B
+    IOR,
+
+    // C = load B bytes from A
+    ILD,
+    // store value in reg A to mem loc stored in B
+    IST,
+    // B = space on stack for A bytes
+    IALLOC,
+    // C = A < B
+    ILT,
+    // C = A < B
+    ILE,
+    // C = A < B
+    IGT,
+    // C = A < B
+    IGE,
+    // C = A < B
+    IEQ,
+    // C = A < B
+    INE,
+    // B = A
+    ICP,
+
+    // C = PHI(a, b). Created by the SSAifier.
+    IPHI,
+};
+
+enum operand {
+    OPER_ILIT,
+    OPER_FLIT,
+    OPER_REG,
+    OPER_LABEL
+};
+
+struct operand {
+    union {
+        struct insn *reg;
+        struct cir_bb *label;
+        uint64_t ilit;
+        double flit;
+    };
+};
+
+enum cir_ty {
+    TY_INT,
+    TY_REAL,
+    TY_ADDR,
+};
+
+// a simple three-address instruction
+struct insn {
+    enum cir_op op;
+    enum cir_ty ty;
+    struct operand a, b, c;
+};
+
+struct cir_bb {
+    ptrvec *insns;
+};
+
+struct cir_func {
+    ptrvec *args;
+    struct cir_bb *entry;
+};
+
+struct cir_prog {
+    ptrvec *funcs;
+    struct cir_func *main;
+};
+
+struct cir_prog *cir_prog();
+struct cir_prog *cir_func();
+struct cir_prog *cir_bb();
+struct insn *insn(enum cir_op, ...);
 
 #endif
